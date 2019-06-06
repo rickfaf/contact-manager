75d24db0d77ff91ac8f4cb4a511dfefda3ca9c44
rickfaf <rickfaf1@gmail.com>
2019-06-06 03:01:20 +0100

    Init project and create Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+          <h4>John Doe</h4>
+          <ul>
+            <li>Email: jde@gmail.com</li>
+            <li>Phone Number: 3323-233-23323</li>
+          </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact
