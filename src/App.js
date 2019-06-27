42baa93652850babe269eabacb024570c8350eaf
rickfaf <rickfaf1@gmail.com>
2019-06-27 19:13:11 +0100

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react"
-import "./App.css"
 import Contact from "./components/Contact"
 import Header from "./components/Header"
-import logo from "./logo.svg"
+// import logo from "./logo.svg"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="John Doe" email="johndoe@gmail.com" phone="553-3333-7887" />
-      <Contact name="Jane Doe" email="janedoe@gmail.com" phone="111-3333-7887" />
+      <div className="container">
+        <Contact name="John Doe" email="johndoe@gmail.com" phone="553-3333-7887" />
+        <Contact name="Jane Doe" email="janedoe@gmail.com" phone="111-3333-7887" />
+      </div>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ import React from "react"
 function Header(props) {
   const { branding } = props
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">{branding}</a>
+        <ul className="navbar-nav">
+          <li class="nav-item">
+            <a href="/" className="nav-link">Home <span className="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
